1c26b1d2d3c68da28bddc8a55adfce3caa3288e7
Rian CD <20231ireads0009@ifba.edu.br>
2025-06-25 22:07:34 -0300

    Add: GetResponseDto for User entity, ObjectMapperUtil class. and application.properties configuration
    ObjectMapperUtil is used to turn into an object type in another type.
    application.properties is used to manage database.

--- estude/src/main/java/com/rian/estude/user/dto/UserGetResponseDto.java
+++ estude/src/main/java/com/rian/estude/user/dto/UserGetResponseDto.java
@@ -2,10 +2,6 @@ package com.rian.estude.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rian.estude.user.enuns.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,33 +11,27 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserPostRequestDto {
+public class UserGetResponseDto {
+
     @JsonProperty(value = "firstName")
-    @NotBlank(message = "First name is required")
     private String firstName;
 
     @JsonProperty(value = "lastName")
     private String lastName;
 
     @JsonProperty(value = "nickname")
-    @NotBlank(message = "nickname is a Required field")
     private String nickname;
 
     @JsonProperty(value = "phone")
     private String phoneNumber;
 
     @JsonProperty(value = "email")
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
     private String email;
 
     @JsonProperty(value = "role")
-    @NotNull(message = "Required field")
     private Role role;
 
     @JsonProperty(value = "password")
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @JsonProperty(value = "biography")
